135f3910b18a1165fc65857f4665c4eaa8878eed
Cameron Dimitroff <hawtrods@gmail.com>
2018-05-21 15:52:21 -0700

    mlab add

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+mongoose.connect("mongodb://q:q@ds231070.mlab.com:31070/heroku_qj5xvkl1");
 
 // Routes
 
